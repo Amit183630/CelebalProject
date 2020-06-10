7e1c899e236082a95820d905fc29a5e055fb1976
Amit Kumar Gupta <Amit183630@gmail.com>
2020-06-10 11:18:15 +0530

    Celebal Project

--- src/restaurant.module.js
+++ src/restaurant.module.js
@@ -3,14 +3,7 @@
   .config(config);
   config.$inject=['$urlRouterProvider'];
   function config($urlRouterProvider) {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if(user){
-        console.log(firebase.auth().currentUser);
         $urlRouterProvider.otherwise('/');
-      }
-      else{
-        $urlRouterProvider.otherwise('/login');
-      }
     });
   }
 })();
